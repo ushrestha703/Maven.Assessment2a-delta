37f11c1677559bcccf10d2bd1cf3e655e30a8e04
Ujjwal Shrestha <ushrestha@zipcoders-MBP-5.localdomain>
2020-03-02 11:15:24 -0500

    String Utils done. Fundamentals completed. 67 test total passed.

--- src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
@@ -6,7 +6,7 @@ package rocks.zipcode.assessment2.fundamentals;
 public class StringUtils {
     /**
      * @param stringToBePadded - string value to be flushed right
-     * @param amountOfPadding - amount of padding to be flushed left
+     * @param amountOfPadding  - amount of padding to be flushed left
      * @return `stringToBePadded` flushed right by left-padding
      */
     public static String padLeft(String stringToBePadded, int amountOfPadding) {
@@ -22,7 +22,7 @@ public class StringUtils {
 
     /**
      * @param stringToBePadded - string value to be flushed left
-     * @param amountOfPadding - amount of padding to be flushed right
+     * @param amountOfPadding  - amount of padding to be flushed right
      * @return `stringToBePadded` flushed right by right-padding
      */
     public static String padRight(String stringToBePadded, int amountOfPadding) {
@@ -34,16 +34,20 @@ public class StringUtils {
             sb.append(' ');
         }
         return sb.toString();
-
     }
 
     /**
-     * @param stringToBeRepeated - string value to be repeated
+     * @param stringToBeRepeated   - string value to be repeated
      * @param numberOfTimeToRepeat - number of times to repeat `stringToBeRepeated`
      * @return the string repeated and concatenated `n` times
      */
     public static String repeatString(String stringToBeRepeated, int numberOfTimeToRepeat) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numberOfTimeToRepeat; i++) {
+            sb.append(stringToBeRepeated);
+        }
+        return sb.toString();
     }
 
     /**
@@ -51,7 +55,16 @@ public class StringUtils {
      * @return - true if string only contains alpha characters
      */
     public static Boolean isAlphaString(String string) {
-        return null;
+        if (string == null) {
+            return false;
+        }
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (!(c >= 'A' && c <= 'Z') && !(c >= 'a' && c <= 'z') && !(c == ' ')) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -59,7 +72,16 @@ public class StringUtils {
      * @return - true if string only contains numeric characters
      */
     public static Boolean isNumericString(String string) {
-        return null;
+        if (string == null) {
+            return false;
+        }
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -67,6 +89,28 @@ public class StringUtils {
      * @return - true if string only contains special characters
      */
     public static Boolean isSpecialCharacterString(String string) {
-        return null;
+
+        for (Character c : string.toCharArray()) {
+            if (Character.isDigit(c) || Character.isAlphabetic(c) || Character.isSpaceChar(c)) {
+                return false;
+            }
+        }
+        return true;
     }
+
+
+     //*********************************
+
+      //  return (string == null) ? false : string.matches("[^A-Za-z0-9 ]");
+        //********************************
+
+//        if (isAlphaString(string) || isNumericString(string)) {
+//            return false;
+//        }
+//        else
+//            return true;
+
+
 }
+
+
