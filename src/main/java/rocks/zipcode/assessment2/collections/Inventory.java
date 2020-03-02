5d27d0662e14cfda349df822b2332d09d972f572
Ujjwal Shrestha <ushrestha@zipcoders-MBP-5.localdomain>
2020-03-02 11:46:57 -0500

    Inventory done. 7 Test passed.

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,37 +1,46 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
+    HashMap<String,Integer> storeInventory;
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
-
+        storeInventory = new HashMap<>();
+        for (String product : strings){
+            addItemToInventory(product);
+        }
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+    storeInventory = new HashMap<>();
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        Integer currentAmt = getItemQuantity(item);
+        currentAmt++;
+        storeInventory.put(item, currentAmt);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        Integer currentAmt = getItemQuantity(item);
+        currentAmt--;
+        storeInventory.put(item, currentAmt);
     }
 
     /**
@@ -39,6 +48,10 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        Integer quantity = 0;
+        if (storeInventory.get(item) != null){
+            quantity = storeInventory.get(item);
+        }
+        return quantity;
     }
 }
